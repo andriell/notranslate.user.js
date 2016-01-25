8f8e0778ef76793c5d0da9f7bc07f1992c7d47d9
andriell <root.andriell@gmail.com>
2016-01-25 18:48:40 +0300

    start

---
+++ notranslate.user.js
@@ -0,0 +1,18 @@
+// ==UserScript==
+// @name notranslate
+// @namespace https://github.com/andriell
+// @description Мой самый первый юзерскрипт
+// @author Рыбалко А.М.
+// @license MIT
+// @version 1.0
+// @include *
+// ==/UserScript==
+(function (window, undefined) {
+    function addClass(items) {
+        for(var i = 0; i < items.length; i++) {
+            items[i].className += " notranslate";
+        }
+    }
+    addClass(document.getElementsByTagName('code'));
+    addClass(document.getElementsByClassName('code'));
+})(window);
